aa845b3569e1d8cca24b040ba5667eb09c244a77
Luis <ltluistrujillo25@gmail.com>
2020-05-23 18:25:25 -0700

    Added splash screen

--- app/src/main/java/com/example/android/bucket_quest/activities/SplashActivity.kt
+++ app/src/main/java/com/example/android/bucket_quest/activities/SplashActivity.kt
@@ -1,22 +1,43 @@
 package com.example.android.bucket_quest.activities
 
-import androidx.appcompat.app.AppCompatActivity
+import android.content.Intent
 import android.os.Bundle
+import android.os.Handler
+import android.view.WindowManager
+import androidx.appcompat.app.AppCompatActivity
 import com.example.android.bucket_quest.R
-import java.lang.NullPointerException
+
 
 class SplashActivity : AppCompatActivity() {
 
+    private val SPLASH_SCREEN_TIME_OUT = 2500
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        window.setFlags(
+            WindowManager.LayoutParams.FLAG_FULLSCREEN,
+            WindowManager.LayoutParams.FLAG_FULLSCREEN
+        )
 
-        try{
-            this.supportActionBar?.hide()
+        // This method is used so that your splash activity
+        // can cover the entire screen.
+        setContentView(R.layout.activity_main)
 
-        }
-        catch (e: NullPointerException){
+        // This will bind your MainActivity.class file with activity_main.
+        Handler().postDelayed({
+            // Intent is used to switch from one activity to another.
+            val i = Intent(
+                this@SplashActivity,
+                MainActivity::class.java
+            )
 
-        }
+            // Invoke the SecondActivity.
+            startActivity(i)
+
+            // The current activity will get finished.
+            finish()
+        }, SPLASH_SCREEN_TIME_OUT.toLong())
         setContentView(R.layout.activity_splash_screen)
+        supportActionBar?.hide()
     }
 }
